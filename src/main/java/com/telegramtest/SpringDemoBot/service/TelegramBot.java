a322ded488ce08a57a5da8bb5f8e85a35dd375f5
Azobioz <145475512+Azobioz@users.noreply.github.com>
2024-12-23 00:19:44 +0300

    commit

--- src/main/java/com/telegramtest/SpringDemoBot/service/TelegramBot.java
+++ src/main/java/com/telegramtest/SpringDemoBot/service/TelegramBot.java
@@ -1,6 +1,7 @@
 package com.telegramtest.SpringDemoBot.service;
 
 import com.telegramtest.SpringDemoBot.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -8,6 +9,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
+@Slf4j
 public class TelegramBot extends TelegramLongPollingBot  {
 
     final BotConfig config;
@@ -42,6 +44,7 @@ public class TelegramBot extends TelegramLongPollingBot  {
 
     public void startCommandReceived(long chatId, String name) {
         String answear = "Hi " + name + ", it's a test bot";
+        log.info("Replied to user " + name);
 
         sendMessage(chatId, answear);
     }
@@ -55,7 +58,7 @@ public class TelegramBot extends TelegramLongPollingBot  {
             execute(message);
         }
         catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+            log.error("Error: " + e.getMessage());
         }
     }
 
