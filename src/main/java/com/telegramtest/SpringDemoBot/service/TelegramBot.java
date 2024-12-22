2837c8f8ff0e024ba830edb83977fa786ba450bc
Azobioz <145475512+Azobioz@users.noreply.github.com>
2024-12-22 16:21:06 +0300

    commit

---
+++ src/main/java/com/telegramtest/SpringDemoBot/service/TelegramBot.java
@@ -0,0 +1,66 @@
+package com.telegramtest.SpringDemoBot.service;
+
+import com.telegramtest.SpringDemoBot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot  {
+
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+
+                default:
+                    sendMessage(chatId, "Command not found");
+                    break;
+            }
+        }
+    }
+
+    public void startCommandReceived(long chatId, String name) {
+        String answear = "Hi " + name + ", it's a test bot";
+
+        sendMessage(chatId, answear);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        }
+        catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+}
---
+++ src/test/java/com/telegramtest/SpringDemoBot/SpringDemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegramtest.SpringDemoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
